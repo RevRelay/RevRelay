a3afcc0266e3e834966841765f9bdb63829f574f
Ernst Aine <vilycharles72@gmail.com>
2021-12-29 11:25:30 -0500

    server side chat code

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+app.use(cors);
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+       origin:"http://localhost:3000",
+       methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) =>{
+console.log(`User Connected: ${socket.id}`);
+
+socket.on("join_room", (data) =>{
+    socket.join(data);
+    console.log(`User ID: ${socket.id} Join room: ${data}`)
+});
+socket.on("disconected", () =>{
+    console.log("User Disconnected", socket.id);
+});
+});
+
+server.listen(3001, () =>{
+    console.log("SERVER Running now");
+});
