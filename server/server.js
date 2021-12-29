01ba0b3629d47aa6fcae6586c45ae81dcce5017f
Krakenmeister <johndmeo@gmail.com>
2021-12-29 22:38:01 +0000

    Integrated chatroom with main app

--- server/server.js
+++ server/server.js
@@ -5,34 +5,34 @@ const {Server} = require("socket.io");
 const cors = require("cors");
 app.use(cors);
 const server = http.createServer(app);
+
+const config = require('../src/config.json');
+
 const io = new Server(server, {
-    cors:{
-       origin:"http://localhost:3000",
-       methods: ["GET", "POST"],
-    },
+	cors: {
+		origin: `${config["URL"]}:${config["APP_PORT"]}`,
+		methods: ["GET", "POST"],
+	},
 });
 
 io.on("connection", (socket) =>{
-console.log(`User Connected: ${socket.id}`);
+	console.log(`User Connected: ${socket.id}`);
 
-socket.on("join_room", (data) =>{
-    socket.join(data);
-    console.log(`User ID: ${socket.id} Join room: ${data}`)
-    });
+	socket.on("join_room", (data) =>{
+		socket.join(data);
+		console.log(`User ID: ${socket.id} Join room: ${data}`)
+	});
 
-    socket.on("message_sent", (data) =>{
-console.log(`message from ${socket.id} ${data}`)
+	socket.on("message_sent", (data) =>{
+		console.log(`message from ${socket.id} ${data}`)
+	});
 
-    });
-
-
-
-socket.on("disconected", () =>{
-    console.log("User Disconnected", socket.id);
-});
+	socket.on("disconected", () =>{
+		console.log("User Disconnected", socket.id);
+	});
 });
 
-server.listen(3001, () =>{
-    console.log("SERVER Running");
+server.listen(config["SOCKET_PORT"], () =>{
+	console.log("SERVER Running");
 });
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Chat from './chatroom/Chat';
 import reportWebVitals from './reportWebVitals';
 import * as router from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<router.BrowserRouter>
-			<App />
+			<router.Routes>
+				<router.Route path="/" element={<App />}/>
+				<router.Route path="chatroom" element={<Chat />}/>
+			</router.Routes>
 		</router.BrowserRouter>
 	</React.StrictMode>,
  	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
