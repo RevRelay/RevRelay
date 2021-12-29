88b6af04104db4c41cee4bd8717e0a6bbe655b5b
rphaynes <ryan.haynes@revature.net>
2021-12-29 12:04:27 -0500

    Message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import {
+	AppBar,
 	Autocomplete,
+	Box,
 	Button,
 	Card,
+	Drawer,
+	IconButton,
 	InputLabel,
 	MenuItem,
 	Select,
-	TextField,
+	List,
+	ListItem,
+	Divider,
+	ListItemText,
+	Toolbar,
+	ListItemIcon,
+	Typography,
 } from "@mui/material";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./Styles/themes.css";
 //https://gridfiti.com/aesthetic-color-palettes/
 //#461E52 | #DD517F | #E68E36 | #556DC8 | #7998EE.
@@ -26,20 +39,96 @@ const themes = [
 ];
 
 function Color(cid, theme) {
-	return "var( --" + cid + "-" + themes[theme] + ")";
+	return "var( --" + cid + "-" + themes[theme] + ") ";
 }
 
 function App() {
 	const [theme, updateTheme] = useState(0);
+	const [sidebar, updateSidebar] = React.useState(false);
+
+	const toggleDrawer = (open) => (event) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		updateSidebar(open);
+	};
 
 	return (
-		<>
-			<div className="App">
-				<InputLabel id="demo-simple-select-label">Age</InputLabel>
+		<React.Fragment style={{ color: Color(3, theme) }}>
+			<div
+				className="App"
+				style={{
+					height: "100vh",
+					minHeight: "100vh",
+					backgroundColor: Color(1, theme),
+					color: Color(3, theme),
+				}}
+			>
+				<Drawer open={sidebar} onClose={toggleDrawer(false)}>
+					<Box
+						sx={{ width: 250, height: "100%" }}
+						role="presentation"
+						onClick={toggleDrawer(false)}
+						onKeyDown={toggleDrawer(false)}
+						style={{ backgroundColor: Color(2, theme) }}
+					>
+						<List>
+							{["Inbox", "Starred", "Send email", "Drafts"].map(
+								(text, index) => (
+									<ListItem button key={text}>
+										<ListItemIcon>
+											{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+										</ListItemIcon>
+										<ListItemText primary={text} />
+									</ListItem>
+								)
+							)}
+						</List>
+						<Divider />
+						<List>
+							{["All mail", "Trash", "Spam"].map((text, index) => (
+								<ListItem button key={text}>
+									<ListItemIcon>
+										{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+									</ListItemIcon>
+									<ListItemText primary={text} />
+								</ListItem>
+							))}
+						</List>
+					</Box>
+				</Drawer>
+				<Box sx={{ flexGrow: 1 }}>
+					<AppBar
+						position="static"
+						style={{ backgroundColor: Color(2, theme) }}
+					>
+						<Toolbar>
+							<IconButton
+								size="large"
+								edge="start"
+								color="inherit"
+								aria-label="menu"
+								sx={{ mr: 2 }}
+								onClick={toggleDrawer(true)}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+								RevRelay
+							</Typography>
+							<Button color="inherit">Register</Button>
+							<Button color="inherit">Login</Button>
+						</Toolbar>
+					</AppBar>
+				</Box>
+
 				<Select
 					labelId="demo-simple-select-label"
 					id="demo-simple-select"
-					label="Theme"
 					defaultValue={0}
 					onChange={(x) => {
 						updateTheme(x.target.value);
@@ -102,7 +191,7 @@ function App() {
 					5
 				</Card>
 			</div>
-		</>
+		</React.Fragment>
 	);
 }
 
