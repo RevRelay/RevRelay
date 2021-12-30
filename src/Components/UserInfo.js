82ace9b1c3420ea5a0eac1c9261fa7a129193541
Jennica LeClerc <jennica@MacBook-Air.local>
2021-12-30 10:22:54 -0500

    Added icons

---
+++ src/Components/UserInfo.js
@@ -0,0 +1,28 @@
+import {
+	AppBar,
+	Autocomplete,
+	Box,
+	Button,
+	Card,
+	Drawer,
+	IconButton,
+	InputLabel,
+	MenuItem,
+	Select,
+	List,
+	ListItem,
+	Divider,
+	ListItemText,
+	Toolbar,
+	ListItemIcon,
+	Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate, Link } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
+import InfoIcon from '@mui/icons-material/Info';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import EditIcon from '@mui/icons-material/Edit';
+import EmailIcon from '@mui/icons-material/Email';
+import EventIcon from '@mui/icons-material/Event';
+import PhotoIcon from '@mui/icons-material/Photo';
