6613d524ba851ef89442e5e90b84b0a84d238c0e
Nathan Luther <nathan.luther@revature.net>
2022-01-01 19:56:55 -0500

    Altered Login and Register to incorporate Mat UI.

--- src/Components/UserInfo/UserInfoEntryElement.js
+++ src/Components/UserInfo/UserInfoEntryElement.js
@@ -44,50 +44,50 @@ import APIQuery from "../../API/APIQuery";
  * @param {Object} props
  * @param {string} props.varname - the variable name associated with the list element (i.e. username).
  * @param {string} props.fieldName - the display name of the list element (i.e. Username).
- * @param {*} props.userInput - state variable holding user field information.
- * @param {*} props.setUserInput - state variable setter for user field information.
- * @param {*} props.toggleEdit - state variable for determining if a field is toggled to display (false) or edit (true). 
- * @param {*} props.setToggleEdit - state variable setter for field toggle state. 
+ * @param {Object} props.userInput - state variable holding user field information.
+ * @param {Function} props.setUserInput - state variable setter for user field information.
+ * @param {Object} props.toggleEdit - state variable for determining if a field is toggled to display (false) or edit (true). 
+ * @param {Function} props.setToggleEdit - state variable setter for field toggle state. 
  * @returns ReactFragment containing UserInfo data with toggles (and eventually editing ability) formatted for insertion into a grid. 
  */
 export default function UserInfoEntryElement ({varname, fieldName, userInput, setUserInput, toggleEdit, setToggleEdit}) {
     return (
 		<React.Fragment>
-				<Grid item xs={3}>
-					<Typography style={{ fontWeight: 600 }}>
-						{fieldName}
-					</Typography>
-				</Grid>
-				{toggleEdit[varname] ? (
-					<React.Fragment>
-						<Grid item xs={2}>
-							<Typography>
-								{(varname == 'password') ? '\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022' : userInput[varname]}
-							</Typography>
-						</Grid>
-						<Grid item xs={1}>
-							<IconButton size="small">
-								<CancelIcon  fontSize="inherit" onClick={(x) => setToggleEdit({...toggleEdit, [varname] : false})}/>
-							</IconButton>
-							<IconButton size="small">
-								<CheckCircleIcon  fontSize="inherit"/>
-							</IconButton>
-						</Grid>
-						<Grid item xs={6}/>
-					</React.Fragment>
-					) : (
-					<React.Fragment>
-						<Grid item xs={2}>
-							<Typography>
-							{(varname == 'password') ? '\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022' : userInput[varname]}
-							</Typography>
-						</Grid>
-						<Grid item xs={7}>
-							<IconButton size="small">
-								<EditIcon  fontSize="inherit" onClick={(x) => setToggleEdit({...toggleEdit, [varname] : true})}/>
-							</IconButton>
-						</Grid>
-					</React.Fragment> 
+			<Grid item xs={3}>
+				<Typography style={{ fontWeight: 600 }}>
+					{fieldName}
+				</Typography>
+			</Grid>
+			{toggleEdit[varname] ? (
+				<React.Fragment>
+					<Grid item xs={2}>
+						<Typography>
+							{(varname === 'password') ? '\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022' : userInput[varname]}
+						</Typography>
+					</Grid>
+					<Grid item xs={1}>
+						<IconButton size="small">
+							<CancelIcon  fontSize="inherit" onClick={(x) => setToggleEdit({...toggleEdit, [varname] : false})}/>
+						</IconButton>
+						<IconButton size="small">
+							<CheckCircleIcon  fontSize="inherit"/>
+						</IconButton>
+					</Grid>
+					<Grid item xs={6}/>
+				</React.Fragment>
+				) : (
+				<React.Fragment>
+					<Grid item xs={2}>
+						<Typography>
+						{(varname === 'password') ? '\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022' : userInput[varname]}
+						</Typography>
+					</Grid>
+					<Grid item xs={7}>
+						<IconButton size="small">
+							<EditIcon  fontSize="inherit" onClick={(x) => setToggleEdit({...toggleEdit, [varname] : true})}/>
+						</IconButton>
+					</Grid>
+				</React.Fragment> 
 					)
 			}
 		</React.Fragment>
