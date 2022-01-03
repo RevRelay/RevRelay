76c5bb4664d9f9362f8367b920b538323be48463
Nathan Luther <nathan.luther@revature.net>
2022-01-03 12:17:58 -0500

    search bar on page all ready to submit to axios.

--- src/Components/Nav/NavSearchBar.js
+++ src/Components/Nav/NavSearchBar.js
@@ -18,19 +18,84 @@ import {
 	Typography,
     TextField
 } from "@mui/material";
-import Color from "../Color.js";
 import React, { useState } from "react";
+import { styled, alpha } from '@mui/material/styles';
+import InputAdornment from '@mui/material/InputAdornment';
 import { Routes, Route, useNavigate, Link } from "react-router-dom";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Logout } from "@mui/icons-material";
-import SidebarList from "./SidebarList.js";
+import SearchIcon from '@mui/icons-material/Search';
+
+// From https://mui.com/components/app-bar/
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+}));
+
+// From https://mui.com/components/app-bar/
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+// From https://mui.com/components/app-bar/
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//     color: 'inherit',
+//     '& .MuiInputBase-input': {
+//         padding: theme.spacing(1, 1, 1, 0),
+//         // vertical padding + font size from searchIcon
+//         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//         transition: theme.transitions.create('width'),
+//         width: '100%',
+//         [theme.breakpoints.up('md')]: {
+//         width: '20ch',
+//         },
+//     },
+// }));
 
 export default function NavSearchBar () {
+    const [searchInput, setSearchInput] = useState({
+        searchInputTerm: ''
+    })
+    const handleChangeSearchBar = (event) => {
+        setSearchInput({searchInputTerm: event.target.value});
+    }
+    
+    const handleSearchSubmit = (event) => {
+        //do search submission, redirect to results page.
+    };
+    
     return (
         <React.Fragment>
-            <TextField sx={{display:'flex'}}/>
+            <Search>
+                <TextField 
+                    id="searchbar-text-field"
+                    sx={{display:'flex'}} onChange={handleChangeSearchBar}
+                    InputProps={{
+                        startAdornment:
+                            <InputAdornment position="start">
+                                <IconButton onClick={handleSearchSubmit} edge="end" >
+                                    <SearchIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    }
+                />
+            </Search>
         </React.Fragment>
     )
     
