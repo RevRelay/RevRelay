548b8b923594cb9428ce5dfa8652ba80f411fff3
luisloustaunau <luis62civil@gmail.com>
2022-01-06 10:14:43 -0600

    added basic styling to friends

--- src/Components/Page/FriendsTab.js
+++ src/Components/Page/FriendsTab.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import APIQuery from "../../API/APIQuery";
-
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import { ListItemText } from "@mui/material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import InboxIcon from "@mui/icons-material/Inbox";
 
 const FriendsTab = ({currentUsername}) => {
     console.log(currentUsername);
     const [friends, setFriends] = useState([]);
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
     const getAllFriends = async ()=> {
        const response = await APIQuery.get("/pages/friends/" + currentUsername, {
         headers: {
@@ -27,14 +31,24 @@ const FriendsTab = ({currentUsername}) => {
 
     return (
       <>
+        <ListItemButton>
+          <ListItemIcon>
+            <InboxIcon />
+            <ListItemText primary="Friends" />
+          </ListItemIcon>
+        </ListItemButton>
+        {loading ? <>
+            <h3>Loading...</h3>      
+             </>
+        : ''}
         {!loading
-          ?
-          friends.map((friend) => {
+          ? friends.map((friend) => {
               return (
                 <>
-                  <h3>Friend:</h3>
-                  <h2>{friend.username}</h2>
-                  <h2>{friend.email}</h2>
+                  <ListItemButton>
+                    <ListItemText primary={friend.username} />
+                    <ListItemText primary={friend.email} />
+                  </ListItemButton>
                 </>
               );
             })
