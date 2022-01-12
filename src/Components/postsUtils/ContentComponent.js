1f593fd3944fe1b340928e49a89711a6eefcfcd1
rphaynes <ryan.haynes@revature.net>
2022-01-12 10:30:36 -0500

    small fixes

--- src/Components/postsUtils/ContentComponent.js
+++ src/Components/postsUtils/ContentComponent.js
@@ -9,51 +9,55 @@ export default function ContentComponent(contentProps) {
 	let st = contentProps.content;
 	let tmp = st.split(/\s+/);
 	comps = tmp.map((word) => {
-		if (word.startsWith("<i=")) {
-			return (
-				<>
-					<br />
-					<img
-						width="420"
-						src={word.split("<i=")[1].split(">")[0]}
-						alt={word.split("<i=")[1].split(">")[0]}
-					/>
-					<br />
-				</>
-			);
-		} else if (word.startsWith("<v=")) {
-			return (
-				<>
-					<br />
-					<video
-						controls
-						src={word.split("<v=")[1].split(">")[0]}
-						alt={word.split("<v=")[1].split(">")[0]}
-					/>
-					<br />
-				</>
-			);
-		} else if (word.startsWith("<y")) {
-			return (
-				<>
-					<br />
+		try {
+			if (word.startsWith("<i=")) {
+				return (
+					<>
+						<br />
+						<img
+							width="420"
+							src={word.split("<i=")[1].split(">")[0]}
+							alt={word.split("<i=")[1].split(">")[0]}
+						/>
+						<br />
+					</>
+				);
+			} else if (word.startsWith("<v=")) {
+				return (
+					<>
+						<br />
+						<video
+							controls
+							src={word.split("<v=")[1].split(">")[0]}
+							alt={word.split("<v=")[1].split(">")[0]}
+						/>
+						<br />
+					</>
+				);
+			} else if (word.startsWith("<y")) {
+				return (
+					<>
+						<br />
 
-					<iframe
-						allow="fullscreen;"
-						height="315"
-						src={
-							"https://www.youtube.com/embed/" +
-							word.split("<y=")[1].split(">")[0].split("=")[1].split("&")[0]
-						}
-						alt={
-							"https://www.youtube.com/embed/" +
-							word.split("<y=")[1].split(">")[0].split("=")[1].split("&")[0]
-						}
-					/>
-					<br />
-				</>
-			);
-		} else return " " + word;
+						<iframe
+							allow="fullscreen;"
+							height="315"
+							src={
+								"https://www.youtube.com/embed/" +
+								word.split("<y=")[1].split(">")[0].split("=")[1].split("&")[0]
+							}
+							alt={
+								"https://www.youtube.com/embed/" +
+								word.split("<y=")[1].split(">")[0].split("=")[1].split("&")[0]
+							}
+						/>
+						<br />
+					</>
+				);
+			} else return " " + word;
+		} catch (e) {
+			return " " + word;
+		}
 	});
 	return comps;
 }
